1860f96e0817e603716f311452d93422bade3979
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-22 20:35:51 +0100

    Update dataclasses for new models

--- scraper.py
+++ scraper.py
@@ -1,62 +1,34 @@
+import json
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, asdict
 from typing import Dict, List, Optional, Any
-import json
-# from django.db import transaction
-# from offers.models import (
-#     Website,
-#     ExperienceLevel,
-#     Salaries,
-#     Localization,
-#     Offers,
-#     ContractType,
-#     WorkSchedule,
-# )
-import logging
-from datetime import datetime
-
-
-logging.basicConfig(
-    filename="../logs.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
 
 
 @dataclass
-class ParsedWebsite:
-    name: str
-    url: Optional[str] = None
+class WorkType:
+    name: Optional[str] = None
 
 
 @dataclass
-class ParsedWorkSchedule:
-    name: str
+class EmploymentType:
+    name: Optional[str] = None
 
 
 @dataclass
-class ParsedContractType:
-    name: str
+class Experience:
+    name = Optional[str] = None
 
 
 @dataclass
-class ParsedExperienceLevel:
-    name: str
-
-
-@dataclass
-class ParsedSalary:
-    salary_from: Optional[int] = None
-    salary_to: Optional[int] = None
+class Salary:
+    salary_from: Optional[float] = None
+    salary_to: Optional[float] = None
     currency: Optional[str] = None
-    contract_type: List[Optional[ParsedContractType]] = None
-    work_schedule: List[Optional[ParsedWorkSchedule]] = None
-    salary_schedule: Optional[int] = None
-    type: Optional[int] = None
+    schedule: Optional[str] = None
 
 
 @dataclass
-class ParsedLocalization:
+class Addresses:
     country: Optional[str] = None
     city: Optional[str] = None
     region: Optional[str] = None
@@ -66,21 +38,19 @@ class ParsedLocalization:
 @dataclass
 class ParsedOffer:
     title: Optional[str] = None
-    url: Optional[str] = None
     description: Optional[str] = None
-    skills: Optional[List[str]] = None
-    company_name: Optional[str] = None
-    company_logo: Optional[str] = None
+    addresses: Optional[str] = None
     is_remote: Optional[bool] = False
     is_hybrid: Optional[bool] = False
-    is_active: Optional[bool] = True
-    is_promoted: Optional[bool] = False
-    date_created: Optional[str] = None
-    date_finished: Optional[str] = None
-    experience_level: Optional[List[ParsedExperienceLevel]] = None
-    salary: Optional[List[ParsedSalary]] = None
-    website: Optional[ParsedWebsite] = None
-    localizations: Optional[List[ParsedLocalization]] = None
+    apply_form: Optional[str] = None
+    skills: Optional[List[str]] = None
+    salary: Optional[List[Salary]] = None
+    experience: Optional[List[Experience]] = None
+    work_type: Optional[List[WorkType]] = None
+    employment_type: Optional[List[EmploymentType]] = None
+    company_logo: Optional[str] = None
+    url = Optional[str] = None
+    company_name: Optional[str] = None
 
 
 class Scraper(ABC):
@@ -114,82 +84,3 @@ class Scraper(ABC):
     def save_data(parsed_offers: List[ParsedOffer]) -> None:
         for parsed_offer in parsed_offers:
             print(parsed_offer)
-        # for parsed_offer in parsed_offers:
-        #     website, created = Website.objects.get_or_create(
-        #         name=parsed_offer.website.name,
-        #         url=parsed_offer.website.url,
-        #     )
-        #
-        #     experience_levels = []
-        #     if parsed_offer.experience_level:
-        #         for exp_level in parsed_offer.experience_level:
-        #             level, created = ExperienceLevel.objects.get_or_create(
-        #                 name=exp_level.name
-        #             )
-        #             experience_levels.append(level)
-        #
-        #     salaries = []
-        #     if parsed_offer.salary:
-        #         for parsed_salary in parsed_offer.salary:
-        #             salary = Salaries(
-        #                 salary_from=parsed_salary.salary_from,
-        #                 salary_to=parsed_salary.salary_to,
-        #                 currency=parsed_salary.currency,
-        #                 salary_schedule=parsed_salary.salary_schedule,
-        #                 type=parsed_salary.type,
-        #             )
-        #             salary.save()
-        #             if parsed_salary.contract_type:
-        #                 for contract_type in parsed_salary.contract_type:
-        #                     (
-        #                         contract_type_obj,
-        #                         created,
-        #                     ) = ContractType.objects.get_or_create(
-        #                         name=contract_type.name
-        #                     )
-        #                     salary.contract_type.add(contract_type_obj)
-        #             if parsed_salary.work_schedule:
-        #                 for work_schedule in parsed_salary.work_schedule:
-        #                     (
-        #                         work_schedule_obj,
-        #                         created,
-        #                     ) = WorkSchedule.objects.get_or_create(
-        #                         name=work_schedule.name
-        #                     )
-        #                     salary.work_schedule.add(work_schedule_obj)
-        #             salaries.append(salary)
-        #
-        #     localizations = []
-        #     if parsed_offer.localizations:
-        #         for loc in parsed_offer.localizations:
-        #             localization = Localization(
-        #                 country=loc.country,
-        #                 city=loc.city,
-        #                 region=loc.region,
-        #                 street=loc.street,
-        #             )
-        #             localization.save()
-        #             localizations.append(localization)
-        #
-        #     offer = Offers(
-        #         title=parsed_offer.title,
-        #         url=parsed_offer.url,
-        #         description=parsed_offer.description,
-        #         skills=", ".join(parsed_offer.skills) if parsed_offer.skills else None,
-        #         company_name=parsed_offer.company_name,
-        #         company_logo=parsed_offer.company_logo,
-        #         is_remote=parsed_offer.is_remote,
-        #         is_hybrid=parsed_offer.is_hybrid,
-        #         is_active=parsed_offer.is_active,
-        #         date_created=datetime.strptime(parsed_offer.date_created, "%Y-%m-%d")
-        #         if parsed_offer.date_created
-        #         else None,
-        #         date_finished=datetime.strptime(parsed_offer.date_finished, "%Y-%m-%d")
-        #         if parsed_offer.date_finished
-        #         else None,
-        #         website=website,
-        #     )
-        #     offer.save()
-        #     offer.experience_level.set(experience_levels)
-        #     offer.salary.set(salaries)
-        #     offer.localizations.set(localizations)
--- scrapers/strategy_abstract/get_content.py
+++ scrapers/strategy_abstract/get_content.py
@@ -1,13 +1,4 @@
 from abc import abstractmethod, ABC
-# from ...models import PageContent
-import logging
-
-
-logging.basicConfig(
-    filename="../logs.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
 
 
 class GetContentStrategy(ABC):
@@ -29,38 +20,5 @@ class GetContentStrategy(ABC):
     def fetch_content(self):
         pass
 
-    # def save_to_db(self) -> bool:
-    #     logging.info(f"Start saving page content for {self.website}")
-    #     if not self.data:
-    #         logging.info(f"Failed to save page content for {self.website}")
-    #         return False
-    #
-    #     try:
-    #         for content in self.data:
-    #             PageContent.objects.create(
-    #                 content=content,
-    #                 website=self.website,
-    #             )
-    #
-    #         logging.info(f"Page content saved for {self.website}")
-    #         return True
-    #     except Exception as e:
-    #         logging.error(f"Failed to save page content for {self.website}: {e}")
-
     def __len__(self):
         return len(self.data)
-
-    # def save_to_db_json(self, data) -> bool:
-    #     logging.info(f"Start saving page content for {self.website}")
-    #     if not data:
-    #         logging.info(f"Failed to save page content for {self.website}")
-    #         return False
-    #     try:
-    #         PageContent.objects.create(
-    #             content_json=data,
-    #             website=self.website,
-    #         )
-    #         logging.info(f"Page content saved for {self.website}")
-    #         return True
-    #     except Exception as e:
-    #         logging.error(f"Failed to save page content for {self.website}: {e}")
